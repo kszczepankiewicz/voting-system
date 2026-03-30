739803a0b3dd1f9ab083729967d69efbedb8dff0
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-30 10:46:47 +0200

    refactor: add guard clause in function addOption

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const poll = new Map();
 
 const addOption = option => {
   if (!option) return `Option cannot be empty.`;
-  if (!poll.has(option)) {
-    poll.set(option, new Set())
-    return `Option "${option}" added to the poll.`
-  }
-  return `Option "${option}" already exists.`;
+  if (poll.has(option)) return `Option "${option}" already exists.`;
+  poll.set(option, new Set())
+  return `Option "${option}" added to the poll.`
 }
 
 const vote = (option, voterId) => {
