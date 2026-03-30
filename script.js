395d418fad202c46f9020449435eb665c8628f5a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-30 10:22:10 +0200

    refactor: check if option is falsay and remove unnecessary {

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 const poll = new Map();
 
 const addOption = option => {
-  if (option === '') {
-    return `Option cannot be empty.`
-  }
+  if (!option) return `Option cannot be empty.`;
   if (!poll.has(option)) {
-    console.log(option)
     poll.set(option, new Set())
     return `Option "${option}" added to the poll.`
-  } else {
-    return `Option "${option}" already exists.`
   }
+  return `Option "${option}" already exists.`;
 }
 
 const vote = (option, voterId) => {
-  console.log(option, voterId);
-  if (!poll.has(option)) {
-    return `Option "${option}" does not exist.`
-  } else {
-    if (poll.get(option).has(voterId)) {
-      return `Voter ${voterId} has already voted for "${option}".`
-    } else {
+  if (!poll.has(option)) return `Option "${option}" does not exist.`;
+  else {
+    if (poll.get(option).has(voterId)) return `Voter ${voterId} has already voted for "${option}".`;
+    else {
       poll.get(option).add(voterId);
       return `Voter ${voterId} voted for "${option}".`
     }
   }
 }
 
-const displayResults = () => {
-  return `Poll Results:\n${Array.from(poll).map(arr => `${arr[0]}: ${arr[1].size} votes`).join('\n')}`;
-}
+const displayResults = () => `Poll Results:\n${Array.from(poll).map(arr => `${arr[0]}: ${arr[1].size} votes`).join('\n')}`;
 
 addOption('Turkey');
 addOption('Morocco');
